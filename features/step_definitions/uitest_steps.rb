b10582a5e9fdac5eef60306ee27f0463b24f1474
Wallace Harwood <wallace.harwood@manheim.com>
2018-07-17 13:45:26 -0500

    [wallace] fixing chrome driver version, removing sleeps, and updating ID of search field

--- features/step_definitions/uitest_steps.rb
+++ features/step_definitions/uitest_steps.rb
@@ -2,15 +2,11 @@
 
 Given(/^I have loaded the Uitest page$/) do
   @uitest = Uitest.new
-  sleep 5
 end
 
 When(/^I enter some text and click the search button$/) do
   @uitest.home_page.search_bar.set 'Test'
-  #@uitest.home_page.search_bar.send_keys 'Test'
-  sleep 1
   @uitest.home_page.search_button.click
-  sleep 5
 end
 
 Then(/^I should see my search results returned$/) do
--- lib/sites/uitest/pages/home_page.rb
+++ lib/sites/uitest/pages/home_page.rb
@@ -4,7 +4,7 @@ require 'taza/page'
 module Uitest
   class HomePage < ::Taza::Page
 
-    element(:search_bar) { browser.input(name: 'q') }
+    element(:search_bar) { browser.text_field(:id => "lst-ib") }
     element(:search_button) { browser.element(name: 'btnK') }
 
   end
